a5e85eb4c821333c408dfbb061035f0b641e3af7
xiaozzz <philip.20050132@163.com>
2018-10-27 00:11:23 +0800

    add negative thread support

--- index.js
+++ index.js
@@ -86,6 +86,10 @@ async function main() {
         thread = max_thread > 1 ? max_thread - 1 : 1;
         Log.w(`auto set thread to ${thread}`);
     }
+    if (thread < 0) {
+        thread = max_thread;
+        Log.w(`auto set thread to ${thread}`);
+    }
 
     // switch server
     let server;
